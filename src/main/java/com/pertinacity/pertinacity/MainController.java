31e2a48501dce143243c28871737ac9f9d53363a
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2021-11-18 13:36:01 -0500

    Rename files, add counter
    Renamed the main class from original example
    add a counter so the button does something

--- src/main/java/com/pertinacity/pertinacity/MainController.java
+++ src/main/java/com/pertinacity/pertinacity/MainController.java
@@ -3,12 +3,15 @@ package com.pertinacity.pertinacity;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
+    private int counter = 0;
+
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        counter++;
+        welcomeText.setText("Welcome to JavaFX Application!" + counter);
     }
 }
