58bd21e76a4f6b53647f1c38ce054440540f1a98
jackfetkovich <johnfetkovich1014@gmail.com>
2021-11-19 14:54:01 -0500

    audio minor changes

--- App/src/main/java/com/pertinacity/MainController.java
+++ App/src/main/java/com/pertinacity/MainController.java
@@ -20,37 +20,65 @@ public class MainController {
     boolean isRecording = false;
 
     Task<Integer> task = new Task<Integer>() {
-        @Override protected Integer call() throws Exception {
-            long iterations;
-            for (iterations = 0; iterations < Long.MAX_VALUE; iterations++) {
-                if (!isRecording) {
-                    break;
-                }
-                System.out.println("Iteration " + iterations);
+        @Override
+        protected Integer call() throws Exception {
+            AudioFormat format = new AudioFormat(44100, 24, 2, true, true);
+        
+            TargetDataLine line;
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, 
+                format); // format is an AudioFormat object
+            if (!AudioSystem.isLineSupported(info)) {
+                // Handle the error ... 
+                System.err.println("bruh");
             }
-            System.out.println("Done");
-            return (int) iterations;
+            // Obtain and open the line.
+            try {
+                line = (TargetDataLine) AudioSystem.getLine(info);
+                line.open(format);
+                
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                int numBytesRead;
+                byte[] data = new byte[line.getBufferSize() / 5];
+                line.start();
+                for (int i = 0; isRecording; i++) {
+                    numBytesRead = line.read(data, 0, data.length);
+                    System.out.println(numBytesRead);
+                    out.write(data, 0, numBytesRead);
+                }
+                line.close();
+    
+                AudioInputStream inputStream = new AudioInputStream(
+                    new ByteArrayInputStream(out.toByteArray()), format,
+                    out.size());
+                AudioSystem.write(inputStream, AudioFileFormat.Type.WAVE, 
+                    new java.io.File("test.wav"));
+            } catch (LineUnavailableException ex) {
+                ex.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return 0;
         }
     };
 
     Thread t = new Thread(task);
 
-    @FXML 
-    protected void record(){
+    @FXML
+    protected void record() {
         isRecording = true;
-        t.setDaemon(true);
+        // t.setDaemon(true);
         t.start();
     }
 
-
     @FXML
-    protected void stopRecording(){
-        isRecording = false;
-        task.cancel();
+    protected void stopRecording() {
+     
+            isRecording = false;
+        
+        task.cancel(true);
         System.out.println(task);
         t.interrupt();
 
     }
 
-    
 }
