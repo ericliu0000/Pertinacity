5a1c44eb550d3414e815673a377b2fb4b2cc06bc
jackfetkovich <johnfetkovich1014@gmail.com>
2021-12-02 12:37:47 -0500

    minor changes

--- App/src/main/java/com/pertinacity/MainApplication.java
+++ App/src/main/java/com/pertinacity/MainApplication.java
@@ -3,6 +3,7 @@ package com.pertinacity;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,11 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+        stage.getIcons().add(
+                new Image(
+                        MainApplication.class.getResourceAsStream("icon.png")));
+
         stage.setTitle("Pertinacity");
         stage.setScene(scene);
         stage.show();
