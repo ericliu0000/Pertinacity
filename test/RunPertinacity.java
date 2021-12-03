2de95ab01a4c7392127c8d98b940da7a62744590
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2021-12-03 14:59:51 -0500

    Add jar

--- test/RunPertinacity.java
+++ test/RunPertinacity.java
@@ -1,6 +1,8 @@
+import java.nio.file.Paths;
+
 public class RunPertinacity {
     public static void main(String[] args) {
-        String[] command = {"res/jdk-with-fx/bin/java", "-jar", "res/pertinacity.jar"};
+        String[] command = {Paths.get("res", "jdk-with-fx", "bin", "java").toString(), "-jar", Paths.get("res", "pertinacity.jar").toString()};
 
         try {
             Process process = Runtime.getRuntime().exec(command);
