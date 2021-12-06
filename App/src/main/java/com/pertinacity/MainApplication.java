066f73c89bf4fa56120fb63331f0dd2685904d57
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2021-12-06 14:14:34 -0500

    Documentation of application

--- App/src/main/java/com/pertinacity/MainApplication.java
+++ App/src/main/java/com/pertinacity/MainApplication.java
@@ -11,13 +11,13 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Load FXML file of the main window
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
 
-        stage.getIcons().add(
-                new Image(
-                        MainApplication.class.getResourceAsStream("icon.png")));
+        stage.getIcons().add(new Image(MainApplication.class.getResourceAsStream("icon.png")));
 
+        // Set stage title and display to user
         stage.setTitle("Pertinacity");
         stage.setScene(scene);
         stage.show();
